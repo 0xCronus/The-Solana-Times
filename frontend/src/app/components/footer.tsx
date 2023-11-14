f21be2f5154ed1fba2a5481d8c29f2a632a65083
ftNikhil.eth <ftnikhil.eth@gmail.com>
2023-11-15 02:06:54 +0530

    refactor(frontend): improve interface and allows put name on initialize user

---
+++ frontend/src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-300 py-6">
+      <div className="container mx-auto px-4 text-center">
+        {/* Copyright */}
+        <p className="text-sm">© 2024 Solana Blog. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
